73202b949e236db529c5eb0cd7fd994146955972
Smith123Tony <JK.Tony@outlook.com>
2022-12-20 22:15:27 +0800

    PTA-团体程序设计天梯赛-练习集

---
+++ 团体程序设计天梯赛-练习集/practise22-12-20.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+////L1-001 Hello World
+//#include<stdio.h>
+//int main()
+//{
+//    printf("Hello World!");
+//    return 0;
+//}
+
+
+
+////L1-004 º∆À„…„ œŒ¬∂»
+//#include<stdio.h>
+//int main()
+//{
+//    int t = 0;
+//    scanf("%d", &t);
+//    int C = 0;
+//    C = 5 * (t - 32) / 9;
+//    printf("Celsius = %d", C);
+//    return 0;
+//}
+
+
